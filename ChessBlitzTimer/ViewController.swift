be306c74cce2be639b2ea2767d89fbf5561a4a73
Johan Näsvall <89924287+nasvalljohan@users.noreply.github.com>
2022-08-29 13:03:32 +0200

    Initial Commit

---
+++ ChessBlitzTimer/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ChessBlitzTimer
+//
+//  Created by Johan Näsvall on 2022-08-29.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
