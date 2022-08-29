3ee7984f897dd50c1c77e473e09eb345a363bd08
Johan Näsvall <89924287+nasvalljohan@users.noreply.github.com>
2022-08-29 15:35:33 +0200

    init

--- ChessBlitzTimer/ViewController.swift
+++ ChessBlitzTimer/ViewController.swift
@@ -8,12 +8,56 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    var count1: Double = 301.0
+    var count2: Double = 301.0
+    var timer = Timer()
+    var switchBool: Bool = false
+    var countBool: Bool = false
+    @IBOutlet weak var lbltimer1: UILabel!
+    @IBOutlet weak var lblTimer2: UILabel!
+    @IBOutlet weak var btnPlay: UIButton!
+    @IBOutlet weak var btnReset: UIButton!
+    var timeFormatter = DateComponentsFormatter()
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+
+        timeFormatter.allowedUnits = [.minute, .second]
+
+        lbltimer1.text = "5:00"
+        lblTimer2.text = "5:00"
+        
+        timer = Timer.scheduledTimer(withTimeInterval: 1, repeats: true, block: updateTimer1(timer:))
+        
+    }
+    
+    func updateTimer1(timer: Timer?){
+        if switchBool == false {
+            count1 = count1 - 1
+            let timeLeft = timeFormatter.string(from: count1)
+            lbltimer1.text = timeLeft
+        }else{
+            count2 = count2 - 1
+            let timeLeft = timeFormatter.string(from: count2)
+            lblTimer2.text = timeLeft
+        }
+    }
+    
+    @IBAction func onPressPlay(_ sender: Any) {
+        updateTimer1(timer: nil)
+        
+        if switchBool == false{
+            switchBool = true
+        }else {
+            switchBool = false
+        }
     }
 
-
+    @IBAction func onPressReset(_ sender: Any) {
+        
+    }
 }
 
